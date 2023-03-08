1dc1b227e2c306e3b9b5debdf324a490ca784e9a
Adelaide Roguet <33537775+a-roguet@users.noreply.github.com>
2023-03-08 17:15:47 -0600

    Change mkdir bootstrap path

--- workflow/02_ww_sc2_Analysis.sh
+++ workflow/02_ww_sc2_Analysis.sh
@@ -121,7 +121,7 @@ if [[ $workflow == "freyja" ]] || [[ $workflow == "all" ]]; then
 
 
     cd /scratch/projects/SARS-CoV-2/
-    mkdir ./Results/$output/bootstraps/
+    mkdir ./Results/$output/freyja/bootstraps/
     
     # Identify SNPs/barcodes
     for variant in Seq*/freyja/*.tsv; do
@@ -140,7 +140,7 @@ fi
 if [[ $workflow == "database_freyja_run" ]] ; then
 
     cd /scratch/projects/SARS-CoV-2/
-    mkdir ./Results/$output/bootstraps/
+    mkdir ./Results/$output/freyja/bootstraps/
 
     # Identify SNPs/barcodes
     for variant in $seq_folder/freyja/*.tsv; do
